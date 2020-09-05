ba05bf5356d32fceb9cbaf790af827249855e1b7
Navaneethan21 <navaneethan@exam.ly>
2020-09-05 20:54:07 +0530

    3 files modified

--- index.js
+++ index.js
@@ -1,10 +1,6 @@
-
-// Get the modal
-var modal = document.getElementById("id01");
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
-}
+ var count = 0;
+      var buttonElement = document.getElementById("btn");
+      function countIt(){
+        count++;
+        alert(`Button Clicked ${count} times`);
+      } 
